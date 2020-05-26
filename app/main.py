764a52a16f0a864fc94a82e7e0770bf5fc43623b
ksanvat <ksanvat@gmail.com>
2020-05-26 07:45:35 +0300

    import & comment fixes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
-from starlette.exceptions import HTTPException
 from starlette.middleware.cors import CORSMiddleware
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-from apps.db import connect_to_mongo, disconnect_from_mongo
 from config import PROJECT_NAME, ALLOWED_HOSTS
+from apps.db import connect_to_mongo, disconnect_from_mongo
 from apps.api import router as api_router
-
-# from .core.errors import http_422_error_handler, http_error_handler
 
 
 app = FastAPI(title=PROJECT_NAME)
@@ -22,8 +18,5 @@ app.add_middleware(
 
 app.add_event_handler('startup', connect_to_mongo)
 app.add_event_handler('shutdown', disconnect_from_mongo)
-
-# app.add_exception_handler(HTTPException, http_error_handler)
-# app.add_exception_handler(HTTP_422_UNPROCESSABLE_ENTITY, http_422_error_handler)
 
 app.include_router(api_router, prefix='/api')
